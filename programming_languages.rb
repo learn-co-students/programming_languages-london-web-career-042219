a2af523ab892da934a2ddb30016fd3903983e81c
Jason Bamford <47010074+jb-23@users.noreply.github.com>
2019-03-29 20:09:32 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+
+  #binding.pry
+  new_langs = {}
+
+  languages.each do | l_style, l_langs |
+    # we loop once for each style
+    l_langs.each do | langname, langdata |
+      # add language and style
+      if new_langs[langname] == nil
+        new_langs[langname] = { style: [] }
+      end
+      new_langs[langname][:style] << l_style
+      new_langs[langname][:type] = langdata[:type]
+    end
+  end
+
+  #binding.pry
+
+  new_langs
 end
