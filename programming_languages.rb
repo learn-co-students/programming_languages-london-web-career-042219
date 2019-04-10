5656f589834c1b1b3fe14a20e26ede38ffe4bc2a
Aiden leeming <aidenjl193@gmail.com>
2019-04-10 17:13:45 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+  languages[:oo].each do |language, info|
+    hash[language] = {
+      type: info[:type],
+      style: [:oo]
+    }
+  end
+  languages[:functional].each do |language, info|
+    if hash.key?(language)
+      hash[language][:style] << :functional
+    else
+      hash[language] = {
+        type: info[:type],
+        style: [:functional]
+      }
+    end
+  end
+  hash
 end
